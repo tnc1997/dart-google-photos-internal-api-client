8c33949fd1366690ca6dc47188165531b6dd97f9
Thomas Clark <tnc1997@outlook.com>
2026-01-18 09:50:42 +0000

    test: remove placeholder test

--- test/google_photos_internal_api_client_test.dart
+++ test/google_photos_internal_api_client_test.dart
@@ -1,16 +1,1 @@
-import 'package:google_photos_internal_api_client/google_photos_internal_api_client.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
