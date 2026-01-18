70e8091bea78dfe2c700351b4bceddd37302147f
Thomas Clark <tnc1997@outlook.com>
2026-01-18 09:51:17 +0000

    refactor: remove placeholder example

--- example/google_photos_internal_api_client_example.dart
+++ example/google_photos_internal_api_client_example.dart
@@ -1,6 +1,1 @@
-import 'package:google_photos_internal_api_client/google_photos_internal_api_client.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
