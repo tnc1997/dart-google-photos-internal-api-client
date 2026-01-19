9a3434d0b3900666b28ac4e3fcaf906ed5cadaf5
Thomas Clark <tnc1997@outlook.com>
2026-01-19 07:16:31 +0000

    refactor: add rpc id constants

--- lib/google_photos_internal_api_client.dart
+++ lib/google_photos_internal_api_client.dart
@@ -1,2 +1,3 @@
 export 'src/common/constants/extension_id.dart';
+export 'src/common/constants/rpc_id.dart';
 export 'src/google_photos_internal_api_client_base.dart';
---
+++ lib/src/common/constants/rpc_id.dart
@@ -0,0 +1,321 @@
+class RpcId {
+  const RpcId._();
+
+  /// The remote procedure call identifier for the method "/OboeService.GetChallenge".
+  static const oboeServiceGetChallenge = 'zKAP2e';
+
+  /// The remote procedure call identifier for the method "/PhotosAutoAddDataService.PhotosAddAutoAddClusters".
+  static const photosAutoAddDataServicePhotosAddAutoAddClusters = 'ONc6Ob';
+
+  /// The remote procedure call identifier for the method "/PhotosAutoAddDataService.PhotosGetCollectionAutoAddClusters".
+  static const photosAutoAddDataServicePhotosGetCollectionAutoAddClusters = 'KcG0jb';
+
+  /// The remote procedure call identifier for the method "/PhotosClusteringDataService.PhotosAddOrRemoveItemsFromClusters".
+  static const photosClusteringDataServicePhotosAddOrRemoveItemsFromClusters = 'KMcZaf';
+
+  /// The remote procedure call identifier for the method "/PhotosClusteringDataService.PhotosReadClustersById".
+  static const photosClusteringDataServicePhotosReadClustersById = 'sCpehe';
+
+  /// The remote procedure call identifier for the method "/PhotosClusteringDataService.PhotosWebReadUserClusters".
+  static const photosClusteringDataServicePhotosWebReadUserClusters = 'yQelMe';
+
+  /// The remote procedure call identifier for the method "/PhotosCurationDataService.PhotosWebReadCarouselMemories".
+  static const photosCurationDataServicePhotosWebReadCarouselMemories = 'dIx4uf';
+
+  /// The remote procedure call identifier for the method "/PhotosCurationDataService.PhotosWebReadCollectionCuratedItemSets".
+  static const photosCurationDataServicePhotosWebReadCollectionCuratedItemSets = 'UJlKrf';
+
+  /// The remote procedure call identifier for the method "/PhotosCurationDataService.PhotosWebReadCuratedItemSetsById".
+  static const photosCurationDataServicePhotosWebReadCuratedItemSetsById = 'y6aGue';
+
+  /// The remote procedure call identifier for the method "/PhotosCurationDataService.PhotosWebReadGridMemories".
+  static const photosCurationDataServicePhotosWebReadGridMemories = 'l5andd';
+
+  /// The remote procedure call identifier for the method "/PhotosCurationDataService.PhotosWebReadGridMemoriesSummary".
+  static const photosCurationDataServicePhotosWebReadGridMemoriesSummary = 'FwuPzf';
+
+  /// The remote procedure call identifier for the method "/PhotosDownloadDataService.PhotosBatchGetDownloadTask".
+  static const photosDownloadDataServicePhotosBatchGetDownloadTask = 'dnv2s';
+
+  /// The remote procedure call identifier for the method "/PhotosDownloadDataService.PhotosCreateMediaCollectionDownloadTask".
+  static const photosDownloadDataServicePhotosCreateMediaCollectionDownloadTask = 'P3pCwd';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAcceptEnvelope".
+  static const photosFeServicePhotosAcceptEnvelope = 'U6Skxb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAddCollectionEnrichments".
+  static const photosFeServicePhotosAddCollectionEnrichments = 'qVm9z';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAddMediaToEnvelope".
+  static const photosFeServicePhotosAddMediaToEnvelope = 'C2V01c';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAddPendingMediaToLibrary".
+  static const photosFeServicePhotosAddPendingMediaToLibrary = 'Xse31c';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAddReceivedItemsToLibrary".
+  static const photosFeServicePhotosAddReceivedItemsToLibrary = 'V8RKJ';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAddRecipientsToEnvelope".
+  static const photosFeServicePhotosAddRecipientsToEnvelope = 'NXNezb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosAddToAlbum".
+  static const photosFeServicePhotosAddToAlbum = 'E1Cajb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosCreateAlbum".
+  static const photosFeServicePhotosCreateAlbum = 'OXvT9d';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosCreateCollectionComment".
+  static const photosFeServicePhotosCreateCollectionComment = 'HF8OLc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosCreateCreation".
+  static const photosFeServicePhotosCreateCreation = 'SGvy5b';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosCreateEnvelope".
+  static const photosFeServicePhotosCreateEnvelope = 'SFKp8c';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosCreateOrSaveDraft".
+  static const photosFeServicePhotosCreateOrSaveDraft = 'FkZVfb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosCreatePrintingOrder".
+  static const photosFeServicePhotosCreatePrintingOrder = 'HlquLc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosDeleteItems".
+  static const photosFeServicePhotosDeleteItems = 'XwAOJf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetAssistantCards".
+  static const photosFeServicePhotosGetAssistantCards = 'uld3vb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetAssistantSettings".
+  static const photosFeServicePhotosGetAssistantSettings = 'udhOIf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetBurstFrames".
+  static const photosFeServicePhotosGetBurstFrames = 'wgZjtc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetCollection".
+  static const photosFeServicePhotosGetCollection = 'snAcKc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetCollectionMoments".
+  static const photosFeServicePhotosGetCollectionMoments = 'wQ6iqd';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetGeoFenceRestrictions".
+  static const photosFeServicePhotosGetGeoFenceRestrictions = 'Ub27zf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetGuidedCreations".
+  static const photosFeServicePhotosGetGuidedCreations = 'IHz0ke';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetOneUpItem".
+  static const photosFeServicePhotosGetOneUpItem = 'VrseUb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetPartnerSharingInfo".
+  static const photosFeServicePhotosGetPartnerSharingInfo = 'O3G8Nd';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetPrintingOrderById".
+  static const photosFeServicePhotosGetPrintingOrderById = 'Ldcsof';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetPrintingOrdersForUser".
+  static const photosFeServicePhotosGetPrintingOrdersForUser = 'BrZZNe';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetPrintingPhotoData".
+  static const photosFeServicePhotosGetPrintingPhotoData = 'eBPAd';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetPrintingPreview".
+  static const photosFeServicePhotosGetPrintingPreview = 'leLxuc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetPrintingProducts".
+  static const photosFeServicePhotosGetPrintingProducts = 'Ljdjxb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetSearchAutoComplete".
+  static const photosFeServicePhotosGetSearchAutoComplete = 'ZSBAec';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetSharedCollections".
+  static const photosFeServicePhotosGetSharedCollections = 'F2A0H';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetSuggestedBooks".
+  static const photosFeServicePhotosGetSuggestedBooks = 'GfmBI';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserCollections".
+  static const photosFeServicePhotosGetUserCollections = 'Z5xsfc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserItemHistogram".
+  static const photosFeServicePhotosGetUserItemHistogram = 'rJ0tlb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserItems".
+  static const photosFeServicePhotosGetUserItems = 'lcxiM';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserPreferences".
+  static const photosFeServicePhotosGetUserPreferences = 'EzwWhf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserSettings".
+  static const photosFeServicePhotosGetUserSettings = 'EX1ySd';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserSuggestions".
+  static const photosFeServicePhotosGetUserSuggestions = 'CF6Zsd';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosGetUserTrash".
+  static const photosFeServicePhotosGetUserTrash = 'zy0IHe';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosPricePrintingOrder".
+  static const photosFeServicePhotosPricePrintingOrder = 'UwbUEc';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosProposePartnerSharing".
+  static const photosFeServicePhotosProposePartnerSharing = 'VNZYy';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosReadActorsById".
+  static const photosFeServicePhotosReadActorsById = 'eNG3nf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosReadItemsById".
+  static const photosFeServicePhotosReadItemsById = 'EWgK9e';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosRemoveUsersFromEnvelope".
+  static const photosFeServicePhotosRemoveUsersFromEnvelope = 'uxzdke';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosReorderCollection".
+  static const photosFeServicePhotosReorderCollection = 'QD9nKf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosReportLocation".
+  static const photosFeServicePhotosReportLocation = 'f0IEud';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosSearchItems".
+  static const photosFeServicePhotosSearchItems = 'EzkLib';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosSetUserPreferences".
+  static const photosFeServicePhotosSetUserPreferences = 'feDIUb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosSuggestCollectionEnrichments".
+  static const photosFeServicePhotosSuggestCollectionEnrichments = 'Lvy0y';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosSuggestCollectionMedia".
+  static const photosFeServicePhotosSuggestCollectionMedia = 'aII5wf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebAddMediaToEnvelope".
+  static const photosFeServicePhotosWebAddMediaToEnvelope = 'laUYf';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebCreateMediaItems".
+  static const photosFeServicePhotosWebCreateMediaItems = 'mdpdU';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebGetBotGuardDecode".
+  static const photosFeServicePhotosWebGetBotGuardDecode = 'x5vKt';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebGetSecureUrls".
+  static const photosFeServicePhotosWebGetSecureUrls = 'SXol3b';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebGetUserDatabaseKeyset".
+  static const photosFeServicePhotosWebGetUserDatabaseKeyset = 'nQy5td';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebImportDriveItems".
+  static const photosFeServicePhotosWebImportDriveItems = 'SusGud';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebReadFailedVideos".
+  static const photosFeServicePhotosWebReadFailedVideos = 'TLvKMb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebReadItemsByContentHash".
+  static const photosFeServicePhotosWebReadItemsByContentHash = 'swbisb';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebReadPartnerItems".
+  static const photosFeServicePhotosWebReadPartnerItems = 'e9T5je';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebRemoveCollections".
+  static const photosFeServicePhotosWebRemoveCollections = 'nV6Qv';
+
+  /// The remote procedure call identifier for the method "/PhotosFeService.PhotosWebSetFavorite".
+  static const photosFeServicePhotosWebSetFavorite = 'Ftfh0';
+
+  /// The remote procedure call identifier for the method "/PhotosMarsDataService.PhotosWebReadUserCloudLockedFolder".
+  static const photosMarsDataServicePhotosWebReadUserCloudLockedFolder = 'nMFwOc';
+
+  /// The remote procedure call identifier for the method "/PhotosMoviesDataService.PhotosCreateMovie".
+  static const photosMoviesDataServicePhotosCreateMovie = 'nBMW4e';
+
+  /// The remote procedure call identifier for the method "/PhotosMoviesDataService.PhotosGetMovieItem".
+  static const photosMoviesDataServicePhotosGetMovieItem = 'wiVIF';
+
+  /// The remote procedure call identifier for the method "/PhotosPickerDataService.PhotosGetPickingSession".
+  static const photosPickerDataServicePhotosGetPickingSession = 'Rsy4kd';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosCreateOrder".
+  static const photosPrintingDataServicePhotosCreateOrder = 'lXlQjf';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosCreateWallArtOrder".
+  static const photosPrintingDataServicePhotosCreateWallArtOrder = 'KS3Mkd';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetGeoFenceRestrictions".
+  static const photosPrintingDataServicePhotosGetGeoFenceRestrictions = 'JyJ6Vc';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetPrintingStartingPrices".
+  static const photosPrintingDataServicePhotosGetPrintingStartingPrices = 'j9dRxe';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetPrintLayout".
+  static const photosPrintingDataServicePhotosGetPrintLayout = 'G9O27';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetPromotions".
+  static const photosPrintingDataServicePhotosGetPromotions = 'uqVVCe';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetRetailStoreByLocation".
+  static const photosPrintingDataServicePhotosGetRetailStoreByLocation = 'h0flpf';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetSubscriptionsForUser".
+  static const photosPrintingDataServicePhotosGetSubscriptionsForUser = 'umOgU';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetSuggestedPrintLayout".
+  static const photosPrintingDataServicePhotosGetSuggestedPrintLayout = 'L3quDd';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosGetWallArtPreview".
+  static const photosPrintingDataServicePhotosGetWallArtPreview = 'kYFBed';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosPlaceOrder".
+  static const photosPrintingDataServicePhotosPlaceOrder = 'KaEeqd';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosPreparePrintingOrder".
+  static const photosPrintingDataServicePhotosPreparePrintingOrder = 'PszKC';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosSaveDraft".
+  static const photosPrintingDataServicePhotosSaveDraft = 'hwtnoc';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosSaveWallArtDraft".
+  static const photosPrintingDataServicePhotosSaveWallArtDraft = 'FNqZl';
+
+  /// The remote procedure call identifier for the method "/PhotosPrintingDataService.PhotosWebGetPrintingConfig".
+  static const photosPrintingDataServicePhotosWebGetPrintingConfig = 'BTQlOe';
+
+  /// The remote procedure call identifier for the method "/PhotosSettingsDataService.PhotosGetPersonalizationSettings".
+  static const photosSettingsDataServicePhotosGetPersonalizationSettings = 'QW5u2b';
+
+  /// The remote procedure call identifier for the method "/PhotosSettingsDataService.PhotosUpdateCollectionLocationSettings".
+  static const photosSettingsDataServicePhotosUpdateCollectionLocationSettings = 'X8k0cf';
+
+  /// The remote procedure call identifier for the method "/PhotosSettingsDataService.PhotosWebGetAskPhotosSettings".
+  static const photosSettingsDataServicePhotosWebGetAskPhotosSettings = 'fFoMo';
+
+  /// The remote procedure call identifier for the method "/PhotosSharingDataService.PhotosCreateCollectionHeart".
+  static const photosSharingDataServicePhotosCreateCollectionHeart = 'phjOdc';
+
+  /// The remote procedure call identifier for the method "/PhotosStorageQuotaDataService.PhotosGetG1RecommendOffer".
+  static const photosStorageQuotaDataServicePhotosGetG1RecommendOffer = 'fYs1bd';
+
+  /// The remote procedure call identifier for the method "/PhotosStorageQuotaDataService.PhotosGetSmartCleanupSummary".
+  static const photosStorageQuotaDataServicePhotosGetSmartCleanupSummary = 'XDs6zc';
+
+  /// The remote procedure call identifier for the method "/PhotosStorageQuotaDataService.PhotosReadStorageQuota".
+  static const photosStorageQuotaDataServicePhotosReadStorageQuota = 'pyDib';
+
+  /// The remote procedure call identifier for the method "/PhotosStorageQuotaDataService.PhotosWebReadUserItemsBySmartCleanupCategory".
+  static const photosStorageQuotaDataServicePhotosWebReadUserItemsBySmartCleanupCategory = 'omQMf';
+
+  /// The remote procedure call identifier for the method "/PhotosSyncedFolderDataService.PhotosGetActiveOems".
+  static const photosSyncedFolderDataServicePhotosGetActiveOems = 'DW0dDe';
+
+  /// The remote procedure call identifier for the method "/PhotosUpdatesDataService.PhotosWebGetUpdates".
+  static const photosUpdatesDataServicePhotosWebGetUpdates = 'frGlJf';
+
+  /// The remote procedure call identifier for the method "/PhotosUserSuggestionDataService.PhotosGetMergeClustersSuggestions".
+  static const photosUserSuggestionDataServicePhotosGetMergeClustersSuggestions = 'aZ9C3e';
+
+  /// The remote procedure call identifier for the method "/PhotosUserSuggestionDataService.PhotosWebReadSuggestedWallArt".
+  static const photosUserSuggestionDataServicePhotosWebReadSuggestedWallArt = 'DgadKd';
+
+  /// The remote procedure call identifier for the method "/PlusAppActivityLogDataService.ActivityLogCategories".
+  static const plusAppActivityLogDataServiceActivityLogCategories = 'fdg1ge';
+
+  /// The remote procedure call identifier for the method "/PlusAppActivityLogDataService.ActivityLogStream".
+  static const plusAppActivityLogDataServiceActivityLogStream = 'wJLlAb';
+}
