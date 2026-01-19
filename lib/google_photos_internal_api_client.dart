584112de47bf6f74a63e11f02b1924adde78834d
Thomas Clark <tnc1997@outlook.com>
2026-01-19 07:16:14 +0000

    refactor: add extension id constants

--- lib/google_photos_internal_api_client.dart
+++ lib/google_photos_internal_api_client.dart
@@ -1,1 +1,2 @@
+export 'src/common/constants/extension_id.dart';
 export 'src/google_photos_internal_api_client_base.dart';
---
+++ lib/src/common/constants/extension_id.dart
@@ -0,0 +1,321 @@
+class ExtensionId {
+  const ExtensionId._();
+
+  /// The extension identifier for the method "/OboeService.GetChallenge".
+  static const oboeServiceGetChallenge = null;
+
+  /// The extension identifier for the method "/PhotosAutoAddDataService.PhotosAddAutoAddClusters".
+  static const photosAutoAddDataServicePhotosAddAutoAddClusters = 194868628;
+
+  /// The extension identifier for the method "/PhotosAutoAddDataService.PhotosGetCollectionAutoAddClusters".
+  static const photosAutoAddDataServicePhotosGetCollectionAutoAddClusters = 188085089;
+
+  /// The extension identifier for the method "/PhotosClusteringDataService.PhotosAddOrRemoveItemsFromClusters".
+  static const photosClusteringDataServicePhotosAddOrRemoveItemsFromClusters = null;
+
+  /// The extension identifier for the method "/PhotosClusteringDataService.PhotosReadClustersById".
+  static const photosClusteringDataServicePhotosReadClustersById = 239196188;
+
+  /// The extension identifier for the method "/PhotosClusteringDataService.PhotosWebReadUserClusters".
+  static const photosClusteringDataServicePhotosWebReadUserClusters = 418845991;
+
+  /// The extension identifier for the method "/PhotosCurationDataService.PhotosWebReadCarouselMemories".
+  static const photosCurationDataServicePhotosWebReadCarouselMemories = null;
+
+  /// The extension identifier for the method "/PhotosCurationDataService.PhotosWebReadCollectionCuratedItemSets".
+  static const photosCurationDataServicePhotosWebReadCollectionCuratedItemSets = null;
+
+  /// The extension identifier for the method "/PhotosCurationDataService.PhotosWebReadCuratedItemSetsById".
+  static const photosCurationDataServicePhotosWebReadCuratedItemSetsById = null;
+
+  /// The extension identifier for the method "/PhotosCurationDataService.PhotosWebReadGridMemories".
+  static const photosCurationDataServicePhotosWebReadGridMemories = null;
+
+  /// The extension identifier for the method "/PhotosCurationDataService.PhotosWebReadGridMemoriesSummary".
+  static const photosCurationDataServicePhotosWebReadGridMemoriesSummary = null;
+
+  /// The extension identifier for the method "/PhotosDownloadDataService.PhotosBatchGetDownloadTask".
+  static const photosDownloadDataServicePhotosBatchGetDownloadTask = null;
+
+  /// The extension identifier for the method "/PhotosDownloadDataService.PhotosCreateMediaCollectionDownloadTask".
+  static const photosDownloadDataServicePhotosCreateMediaCollectionDownloadTask = null;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAcceptEnvelope".
+  static const photosFeServicePhotosAcceptEnvelope = 100075292;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAddCollectionEnrichments".
+  static const photosFeServicePhotosAddCollectionEnrichments = 104759230;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAddMediaToEnvelope".
+  static const photosFeServicePhotosAddMediaToEnvelope = 99484733;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAddPendingMediaToLibrary".
+  static const photosFeServicePhotosAddPendingMediaToLibrary = 87010787;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAddReceivedItemsToLibrary".
+  static const photosFeServicePhotosAddReceivedItemsToLibrary = 87211250;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAddRecipientsToEnvelope".
+  static const photosFeServicePhotosAddRecipientsToEnvelope = 113803980;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosAddToAlbum".
+  static const photosFeServicePhotosAddToAlbum = 79956622;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosCreateAlbum".
+  static const photosFeServicePhotosCreateAlbum = 200640278;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosCreateCollectionComment".
+  static const photosFeServicePhotosCreateCollectionComment = 98349023;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosCreateCreation".
+  static const photosFeServicePhotosCreateCreation = 103530436;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosCreateEnvelope".
+  static const photosFeServicePhotosCreateEnvelope = 84378251;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosCreateOrSaveDraft".
+  static const photosFeServicePhotosCreateOrSaveDraft = 148097499;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosCreatePrintingOrder".
+  static const photosFeServicePhotosCreatePrintingOrder = 124769329;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosDeleteItems".
+  static const photosFeServicePhotosDeleteItems = 73931313;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetAssistantCards".
+  static const photosFeServicePhotosGetAssistantCards = 90100657;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetAssistantSettings".
+  static const photosFeServicePhotosGetAssistantSettings = 98168207;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetBurstFrames".
+  static const photosFeServicePhotosGetBurstFrames = 117948876;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetCollection".
+  static const photosFeServicePhotosGetCollection = 71837398;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetCollectionMoments".
+  static const photosFeServicePhotosGetCollectionMoments = 145853420;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetGeoFenceRestrictions".
+  static const photosFeServicePhotosGetGeoFenceRestrictions = null;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetGuidedCreations".
+  static const photosFeServicePhotosGetGuidedCreations = 164658508;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetOneUpItem".
+  static const photosFeServicePhotosGetOneUpItem = 74881883;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetPartnerSharingInfo".
+  static const photosFeServicePhotosGetPartnerSharingInfo = 150230321;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetPrintingOrderById".
+  static const photosFeServicePhotosGetPrintingOrderById = 136705160;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetPrintingOrdersForUser".
+  static const photosFeServicePhotosGetPrintingOrdersForUser = 137399189;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetPrintingPhotoData".
+  static const photosFeServicePhotosGetPrintingPhotoData = 197707883;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetPrintingPreview".
+  static const photosFeServicePhotosGetPrintingPreview = 127754195;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetPrintingProducts".
+  static const photosFeServicePhotosGetPrintingProducts = 124573825;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetSearchAutoComplete".
+  static const photosFeServicePhotosGetSearchAutoComplete = 85564020;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetSharedCollections".
+  static const photosFeServicePhotosGetSharedCollections = 83991193;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetSuggestedBooks".
+  static const photosFeServicePhotosGetSuggestedBooks = 153369960;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserCollections".
+  static const photosFeServicePhotosGetUserCollections = 72930366;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserItemHistogram".
+  static const photosFeServicePhotosGetUserItemHistogram = 83286035;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserItems".
+  static const photosFeServicePhotosGetUserItems = 74806772;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserPreferences".
+  static const photosFeServicePhotosGetUserPreferences = 89627394;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserSettings".
+  static const photosFeServicePhotosGetUserSettings = 87031856;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserSuggestions".
+  static const photosFeServicePhotosGetUserSuggestions = 147662530;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosGetUserTrash".
+  static const photosFeServicePhotosGetUserTrash = 73336943;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosPricePrintingOrder".
+  static const photosFeServicePhotosPricePrintingOrder = 148809515;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosProposePartnerSharing".
+  static const photosFeServicePhotosProposePartnerSharing = 148795743;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosReadActorsById".
+  static const photosFeServicePhotosReadActorsById = 215647270;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosReadItemsById".
+  static const photosFeServicePhotosReadItemsById = 112611860;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosRemoveUsersFromEnvelope".
+  static const photosFeServicePhotosRemoveUsersFromEnvelope = 101387681;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosReorderCollection".
+  static const photosFeServicePhotosReorderCollection = 96600466;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosReportLocation".
+  static const photosFeServicePhotosReportLocation = 105073398;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosSearchItems".
+  static const photosFeServicePhotosSearchItems = 82672226;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosSetUserPreferences".
+  static const photosFeServicePhotosSetUserPreferences = 89658435;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosSuggestCollectionEnrichments".
+  static const photosFeServicePhotosSuggestCollectionEnrichments = 102043311;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosSuggestCollectionMedia".
+  static const photosFeServicePhotosSuggestCollectionMedia = 146652045;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebAddMediaToEnvelope".
+  static const photosFeServicePhotosWebAddMediaToEnvelope = 214987827;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebCreateMediaItems".
+  static const photosFeServicePhotosWebCreateMediaItems = 137530650;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebGetBotGuardDecode".
+  static const photosFeServicePhotosWebGetBotGuardDecode = null;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebGetSecureUrls".
+  static const photosFeServicePhotosWebGetSecureUrls = 310227073;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebGetUserDatabaseKeyset".
+  static const photosFeServicePhotosWebGetUserDatabaseKeyset = 294525259;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebImportDriveItems".
+  static const photosFeServicePhotosWebImportDriveItems = 243677339;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebReadFailedVideos".
+  static const photosFeServicePhotosWebReadFailedVideos = 213073696;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebReadItemsByContentHash".
+  static const photosFeServicePhotosWebReadItemsByContentHash = 117028342;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebReadPartnerItems".
+  static const photosFeServicePhotosWebReadPartnerItems = 149705538;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebRemoveCollections".
+  static const photosFeServicePhotosWebRemoveCollections = 144367102;
+
+  /// The extension identifier for the method "/PhotosFeService.PhotosWebSetFavorite".
+  static const photosFeServicePhotosWebSetFavorite = 232568217;
+
+  /// The extension identifier for the method "/PhotosMarsDataService.PhotosWebReadUserCloudLockedFolder".
+  static const photosMarsDataServicePhotosWebReadUserCloudLockedFolder = null;
+
+  /// The extension identifier for the method "/PhotosMoviesDataService.PhotosCreateMovie".
+  static const photosMoviesDataServicePhotosCreateMovie = 226333628;
+
+  /// The extension identifier for the method "/PhotosMoviesDataService.PhotosGetMovieItem".
+  static const photosMoviesDataServicePhotosGetMovieItem = 192213782;
+
+  /// The extension identifier for the method "/PhotosPickerDataService.PhotosGetPickingSession".
+  static const photosPickerDataServicePhotosGetPickingSession = null;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosCreateOrder".
+  static const photosPrintingDataServicePhotosCreateOrder = null;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosCreateWallArtOrder".
+  static const photosPrintingDataServicePhotosCreateWallArtOrder = 238628978;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetGeoFenceRestrictions".
+  static const photosPrintingDataServicePhotosGetGeoFenceRestrictions = 256217573;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetPrintLayout".
+  static const photosPrintingDataServicePhotosGetPrintLayout = 299360643;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetPrintingStartingPrices".
+  static const photosPrintingDataServicePhotosGetPrintingStartingPrices = 235062980;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetPromotions".
+  static const photosPrintingDataServicePhotosGetPromotions = 169420602;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetRetailStoreByLocation".
+  static const photosPrintingDataServicePhotosGetRetailStoreByLocation = 233817700;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetSubscriptionsForUser".
+  static const photosPrintingDataServicePhotosGetSubscriptionsForUser = 275052373;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetSuggestedPrintLayout".
+  static const photosPrintingDataServicePhotosGetSuggestedPrintLayout = 312139251;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosGetWallArtPreview".
+  static const photosPrintingDataServicePhotosGetWallArtPreview = 241727046;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosPlaceOrder".
+  static const photosPrintingDataServicePhotosPlaceOrder = null;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosPreparePrintingOrder".
+  static const photosPrintingDataServicePhotosPreparePrintingOrder = 216575938;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosSaveDraft".
+  static const photosPrintingDataServicePhotosSaveDraft = null;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosSaveWallArtDraft".
+  static const photosPrintingDataServicePhotosSaveWallArtDraft = 246562496;
+
+  /// The extension identifier for the method "/PhotosPrintingDataService.PhotosWebGetPrintingConfig".
+  static const photosPrintingDataServicePhotosWebGetPrintingConfig = null;
+
+  /// The extension identifier for the method "/PhotosSettingsDataService.PhotosGetPersonalizationSettings".
+  static const photosSettingsDataServicePhotosGetPersonalizationSettings = null;
+
+  /// The extension identifier for the method "/PhotosSettingsDataService.PhotosUpdateCollectionLocationSettings".
+  static const photosSettingsDataServicePhotosUpdateCollectionLocationSettings = null;
+
+  /// The extension identifier for the method "/PhotosSettingsDataService.PhotosWebGetAskPhotosSettings".
+  static const photosSettingsDataServicePhotosWebGetAskPhotosSettings = null;
+
+  /// The extension identifier for the method "/PhotosSharingDataService.PhotosCreateCollectionHeart".
+  static const photosSharingDataServicePhotosCreateCollectionHeart = 183910266;
+
+  /// The extension identifier for the method "/PhotosStorageQuotaDataService.PhotosGetG1RecommendOffer".
+  static const photosStorageQuotaDataServicePhotosGetG1RecommendOffer = null;
+
+  /// The extension identifier for the method "/PhotosStorageQuotaDataService.PhotosGetSmartCleanupSummary".
+  static const photosStorageQuotaDataServicePhotosGetSmartCleanupSummary = 311737317;
+
+  /// The extension identifier for the method "/PhotosStorageQuotaDataService.PhotosReadStorageQuota".
+  static const photosStorageQuotaDataServicePhotosReadStorageQuota = 207585746;
+
+  /// The extension identifier for the method "/PhotosStorageQuotaDataService.PhotosWebReadUserItemsBySmartCleanupCategory".
+  static const photosStorageQuotaDataServicePhotosWebReadUserItemsBySmartCleanupCategory = 311737319;
+
+  /// The extension identifier for the method "/PhotosSyncedFolderDataService.PhotosGetActiveOems".
+  static const photosSyncedFolderDataServicePhotosGetActiveOems = null;
+
+  /// The extension identifier for the method "/PhotosUpdatesDataService.PhotosWebGetUpdates".
+  static const photosUpdatesDataServicePhotosWebGetUpdates = null;
+
+  /// The extension identifier for the method "/PhotosUserSuggestionDataService.PhotosGetMergeClustersSuggestions".
+  static const photosUserSuggestionDataServicePhotosGetMergeClustersSuggestions = 191643343;
+
+  /// The extension identifier for the method "/PhotosUserSuggestionDataService.PhotosWebReadSuggestedWallArt".
+  static const photosUserSuggestionDataServicePhotosWebReadSuggestedWallArt = 244762387;
+
+  /// The extension identifier for the method "/PlusAppActivityLogDataService.ActivityLogCategories".
+  static const plusAppActivityLogDataServiceActivityLogCategories = 95167161;
+
+  /// The extension identifier for the method "/PlusAppActivityLogDataService.ActivityLogStream".
+  static const plusAppActivityLogDataServiceActivityLogStream = 96372017;
+}
