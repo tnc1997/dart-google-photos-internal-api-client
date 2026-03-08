a55dc5911d5b1d03cc1d0de016b4055693c0c62f
Thomas Clark <tnc1997@outlook.com>
2026-03-08 16:41:31 +0000

    refactor: add photos read items by id response media item owner model

--- lib/src/photos_fe_service/models/photos_read_items_by_id_response.dart
+++ lib/src/photos_fe_service/models/photos_read_items_by_id_response.dart
@@ -1,3 +1,25 @@
+class PhotosReadItemsByIdResponseMediaItemOwner {
+  PhotosReadItemsByIdResponseMediaItemOwnerIdentifiers? identifiers;
+  String? mediaItemId;
+
+  PhotosReadItemsByIdResponseMediaItemOwner({
+    this.identifiers,
+    this.mediaItemId,
+  });
+
+  factory PhotosReadItemsByIdResponseMediaItemOwner.fromData(
+    List<dynamic> data,
+  ) {
+    return PhotosReadItemsByIdResponseMediaItemOwner(
+      identifiers: data[1] != null
+          ? PhotosReadItemsByIdResponseMediaItemOwnerIdentifiers.fromData(
+              data[1])
+          : null,
+      mediaItemId: data[0],
+    );
+  }
+}
+
 class PhotosReadItemsByIdResponseMediaItemOwnerIdentifiers {
   String? googleAccountsAndIdAdministration;
 
