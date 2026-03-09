fa29e25ee03eab3d346c0cf19bb5294881d33987
Thomas Clark <tnc1997@outlook.com>
2026-03-09 20:54:34 +0000

    refactor: add photos read items by id response media item details storage model

--- lib/src/photos_fe_service/models/photos_read_items_by_id_response.dart
+++ lib/src/photos_fe_service/models/photos_read_items_by_id_response.dart
@@ -1,3 +1,31 @@
+class PhotosReadItemsByIdResponseMediaItemDetailsStorage {
+  bool? doesConsumeStorage;
+  int? highQualityFileSize;
+  bool? isHighQuality;
+  bool? isOriginalQuality;
+  int? originalQualityFileSize;
+
+  PhotosReadItemsByIdResponseMediaItemDetailsStorage({
+    this.doesConsumeStorage,
+    this.highQualityFileSize,
+    this.isHighQuality,
+    this.isOriginalQuality,
+    this.originalQualityFileSize,
+  });
+
+  factory PhotosReadItemsByIdResponseMediaItemDetailsStorage.fromData(
+    List<dynamic> data,
+  ) {
+    return PhotosReadItemsByIdResponseMediaItemDetailsStorage(
+      doesConsumeStorage: data[0] != null ? data[0] == 1 : null,
+      highQualityFileSize: data[3],
+      isHighQuality: data[2] != null ? data[2] == 1 : null,
+      isOriginalQuality: data[2] != null ? data[2] == 2 : null,
+      originalQualityFileSize: data[1],
+    );
+  }
+}
+
 class PhotosReadItemsByIdResponseMediaItemOwner {
   PhotosReadItemsByIdResponseMediaItemOwnerIdentifiers? identifiers;
   String? mediaItemId;
