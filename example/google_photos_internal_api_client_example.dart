ee531ce3304badf95879c165e579b0f7ce7f1976
Thomas Clark <tnc1997@outlook.com>
2026-03-31 08:25:27 +0100

    refactor: add package example

--- example/google_photos_internal_api_client_example.dart
+++ example/google_photos_internal_api_client_example.dart
@@ -1,1 +1,28 @@
-void main() {}
+import 'package:google_photos_internal_api_client/google_photos_internal_api_client.dart';
+
+Future<void> main() async {
+  final client = GooglePhotosInternalApiClient(
+    client: CookieClient.fromCookieHeaderValue(
+      '',
+    ),
+  );
+
+  String? pageId;
+
+  do {
+    final response = await client.photosFeService.photosGetUserCollections(
+      pageId: pageId,
+      pageSize: 100,
+    );
+
+    final albums = response.albums;
+
+    if (albums != null) {
+      for (final album in albums) {
+        print(album.details?.title);
+      }
+    }
+
+    pageId = response.nextPageId;
+  } while (pageId != null);
+}
